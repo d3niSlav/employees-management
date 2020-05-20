248820adb076fa798b002c5620a4d27a136ef198
Denislav Enchev <denislav.enchev@scalefocus.com>
2020-05-20 14:39:55 +0300

    Created modal component.

---
+++ src/components/Modal/constants.js
@@ -0,0 +1,30 @@
+export const MODAL_VARS = {
+  animationDuration: 300,
+  defaultActionsPosition: 'center',
+  defaultColorScheme: 'primary',
+  defaultPosition: 'center',
+  defaultType: 'center',
+  actionsPositions: {
+    left: 'left',
+    center: 'center',
+    right: 'right'
+  },
+  colorSchemes: {
+    primary: 'Primary',
+    secondary: 'Secondary',
+    success: 'Success',
+    error: 'Error',
+    warning: 'Warning',
+    info: 'Info'
+  },
+  positions: {
+    top: 'Top',
+    center: 'Center',
+    bottom: 'Bottom'
+  },
+  types: {
+    confirmation: 'confirmation',
+    information: 'information',
+    custom: 'custom'
+  }
+};
